4f99987d1b9e5919eefe664ff334aebccc6c0157
jsanchoz11 <120020316+jsanchoz11@users.noreply.github.com>
2024-04-30 12:32:46 +0200

    Update create_data.sh
    The user needs to add the path_deepRBP path

--- data/input_create_model/processed/create_data.sh
+++ data/input_create_model/processed/create_data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 SCRIPT_DIR=$(dirname "$0")
 
-path_deepRBP="/Users/joseba/Downloads/ML4BM-Lab2/DeepRBP"
+path_deepRBP="" # Change this path to your DeepRBP folder
 
 export PYTHONPATH="$path_deepRBP/model:$PYTHONPATH"
 PATH_DATA="$path_deepRBP/data/input_create_model"
 
 echo "Ruta de PATH_DATA: $PATH_DATA"
-
+echo $PYTHONPATH
 python "$SCRIPT_DIR/create_data.py" --chunksize 5000 --select_genes 'cancer_genes' --path_data "$PATH_DATA"
