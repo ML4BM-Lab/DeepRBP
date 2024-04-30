12b1ab8702e431228e49d31a2de90ba41c32a1bf
jsanchoz11 <120020316+jsanchoz11@users.noreply.github.com>
2024-04-30 12:58:05 +0200

    Update create_data.sh

--- data/input_create_model/processed/create_data.sh
+++ data/input_create_model/processed/create_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR="$PWD"
 
-path_deepRBP="/scratch/jsanchoz/DeepRBP" # Change this path to your DeepRBP folder
+path_deepRBP="" # Change this path to your DeepRBP folder
 PATH_DATA="$path_deepRBP/data/input_create_model"
 echo "Ruta de PATH_DATA: $PATH_DATA"
 
