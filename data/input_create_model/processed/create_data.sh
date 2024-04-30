e18e7481baa1edd47863a84c6872b50a39c7e485
jsanchoz11 <120020316+jsanchoz11@users.noreply.github.com>
2024-04-30 12:54:45 +0200

    Update create_data.sh
    modify script_dir for executing also with slurm

--- data/input_create_model/processed/create_data.sh
+++ data/input_create_model/processed/create_data.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-SCRIPT_DIR=$(dirname "$0")
+SCRIPT_DIR="$PWD"
 
-path_deepRBP="" # Change this path to your DeepRBP folder
-
-export PYTHONPATH="$path_deepRBP/model:$PYTHONPATH"
+path_deepRBP="/scratch/jsanchoz/DeepRBP" # Change this path to your DeepRBP folder
 PATH_DATA="$path_deepRBP/data/input_create_model"
-
 echo "Ruta de PATH_DATA: $PATH_DATA"
-echo $PYTHONPATH
+
 python "$SCRIPT_DIR/create_data.py" --chunksize 5000 --select_genes 'cancer_genes' --path_data "$PATH_DATA"
